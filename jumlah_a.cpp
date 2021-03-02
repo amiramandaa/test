5110f8fab80d52bd50e186d4e3fe215b8b89d3fd
amiramandaa <52105312+amiramandaa@users.noreply.github.com>
2021-03-02 10:51:36 +0700

    Add files via upload

---
+++ jumlah_a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x= 5;
+	int y= 5;
+
+	cout<<" Penjumlahn x dan y "<<endl;
+	cout<<"y = "<<x<<endl;
+	cout<<"x = "<<y<<endl;
+	cout"\n";
+
+	cout<<"Hasil x + y = "<<x+y<<;
+}
